46a122440762c0430832ee4e657a0ea8f9418951
Sunny <me@darkowlzz.space>
2017-02-06 18:59:20 +0530

    [build] Add terragrunt and use hiera

--- build/manifests/site.pp
+++ build/manifests/site.pp
@@ -9,7 +9,23 @@ archive { '/tmp/terraform.zip':
   ensure       => present,
   extract      => true,
   extract_path => '/usr/local/bin',
-  source       => 'https://releases.hashicorp.com/terraform/0.8.5/terraform_0.8.5_linux_amd64.zip',
+  source       => hiera('terraform_download_link'),
   user         => 'root',
   group        => 'root'
+}
+
+# Install terragrunt
+# Using wget because `file` resource fails to fetch the remote resources with
+# url redirections.
+include wget
+wget::fetch { hiera('terragrunt_download_link'):
+  destination => '/usr/local/bin/terragrunt',
+} -> # make the file executable
+file { '/usr/local/bin/terragrunt':
+  mode => '0755'
+}
+
+file { '/etc/profile.d/terragrunt.sh':
+  ensure  => file,
+  content => "export TERRAGRUNT_TFPATH=/usr/local/bin/terraform",
 }
